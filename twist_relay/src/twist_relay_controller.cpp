52a28ac24e9df19a2efee005923cf9f9c1adf9ff
Bence Magyar <bence.magyar.robotics@gmail.com>
2023-10-17 00:40:52 +0100

    Add twist relay

---
+++ twist_relay/src/twist_relay_controller.cpp
@@ -0,0 +1,106 @@
+// Copyright (c) 2023, ros2_control development team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "twist_relay/twist_relay_controller.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+
+
+namespace
+{
+  const auto DEFAULT_COMMAND_TOPIC = "/cmd_vel";
+  using controller_interface::interface_configuration_type;
+}
+
+namespace twist_relay_controller
+{
+RelayController::RelayController()
+: controller_interface::ControllerInterface()
+{
+}
+
+controller_interface::CallbackReturn RelayController::on_init()
+{
+   return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::InterfaceConfiguration RelayController::command_interface_configuration() const
+{
+  std::vector<std::string> conf_names = {params_.linear_velocity_cmd_if, params_.angular_velocity_cmd_if, params_.steering_angle_cmd_if};
+  return {interface_configuration_type::INDIVIDUAL, conf_names};
+}
+
+  controller_interface::InterfaceConfiguration RelayController::state_interface_configuration() const
+  {
+ return controller_interface::InterfaceConfiguration{
+    controller_interface::interface_configuration_type::NONE};
+  }
+
+
+controller_interface::CallbackReturn RelayController::on_configure(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // update parameters if they have changed
+  if (param_listener_->is_old(params_))
+  {
+    params_ = param_listener_->get_params();
+    RCLCPP_INFO(get_node()->get_logger(), "Parameters were updated");
+  }
+
+  twist_subscriber_ = get_node()->create_subscription<Twist>(
+        DEFAULT_COMMAND_TOPIC, rclcpp::SystemDefaultsQoS(),
+        [this](const std::shared_ptr<Twist> msg) -> void
+        {
+          if (!subscriber_is_active_)
+          {
+            RCLCPP_WARN(
+              get_node()->get_logger(), "Can't accept new commands. subscriber is inactive");
+            return;
+          }
+          if ((msg->header.stamp.sec == 0) && (msg->header.stamp.nanosec == 0))
+          {
+            RCLCPP_WARN_ONCE(
+              get_node()->get_logger(),
+              "Received TwistStamped with zero timestamp, setting it to current "
+              "time, this message will only be shown once");
+            msg->header.stamp = get_node()->get_clock()->now();
+          }
+          last_msg_ptr_.set(std::move(msg));
+        });
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::return_type update(
+  const rclcpp::Time & time, const rclcpp::Duration & period)
+{
+  std::shared_ptr<Twist> last_command_msg;
+  last_msg_ptr_.get(last_command_msg);
+  if(last_msg_ptr_ != nullptr)
+  {
+    command_interfaces_[0].set_value(last_msg_ptr_->twist.linear.x);
+        command_interfaces_[1].set_value(last_msg_ptr_->twist.angular.z);
+        double fake_steering_angle = last_msg_ptr_->twist.angular.z * params_.yaw_multiplier;
+            command_interfaces_[2].set_value(fake_steering_angle);
+  }
+}
+
+
+
+}  // namespace twist_relay_controller
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  twist_relay_controller::RelayController,
+  controller_interface::ControllerInterface)
